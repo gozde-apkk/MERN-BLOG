1ad918a21e954ffacc5920def15cd3e48e92292d
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-21 21:57:28 +0300

    create signup api

--- api/index.js
+++ api/index.js
@@ -4,15 +4,20 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.route.js';
+
 
 dotenv.config();
 const app = express();
+
+app.use(express.json());
 const PORT = 3000;
 mongoose.connect(process.env.MONGO             
 ).then(() => {
     console.log('MongoDB Connected');
 })
-app.use("/api/user" , userRoutes)
+app.use("/api/user" , userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT , () => {
     console.log(`server running on port ${PORT}`);
