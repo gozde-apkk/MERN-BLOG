45d1750f8fa2027594b94e1364d545ff416984a8
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-22 12:26:13 +0300

    firebase config

---
+++ client/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDV_H7qAd-w-JatwvOMlatTQhATyhqFUUU",
+  authDomain: "mern-blo-61f76.firebaseapp.com",
+  projectId: "mern-blo-61f76",
+  storageBucket: "mern-blo-61f76.appspot.com",
+  messagingSenderId: "796862695408",
+  appId: "1:796862695408:web:a29581564c19c5cf32ff35",
+  measurementId: "G-PYCNJR62EC"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server : {
+    proxy : {
+      '/api' : {
+        target: 'http://localhost:3000', // 这里是你的
+        changeOrigin: true,               // needed for virtual hosted sites
+        secure : false
+      }
+    }
+  },
   plugins: [react()],
 })
