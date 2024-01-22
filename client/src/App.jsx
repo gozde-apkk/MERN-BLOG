1af080fc0aed253a33b93445855332d91ba188de
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-22 12:24:42 +0300

    created footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
+import FooterCom from "./components/FooterCom";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <FooterCom/>
     </BrowserRouter>
   );
 }
