886b316b1d99be276c8ddcdf4ed59559745b56ed
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-22 23:55:19 +0300

    user.route

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -2,10 +2,17 @@
 
 import express from 'express';
 import { test } from '../controller/user.controller.js  ';
+import { verifyToken } from '../utils/verifyUser.js';
+import { deleteUser, signout, updateUser } from '../controller/user.controller.js';
 
 
 const router = express.Router();
 
 router.get('/test', test)
+
+router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
+router.post("/signout", signout);
+
 
 export default router;  
