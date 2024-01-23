b4e9357866873a1544df3b96e3d872aa50331260
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-23 10:41:42 +0300

    post routes and create api

--- api/index.js
+++ api/index.js
@@ -3,13 +3,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.route.js';
+import postRouter from './routes/post.route.js'
+
 
 
 dotenv.config();
 const app = express();
-
+app.use(cookieParser());
 app.use(express.json());
 const PORT = 3000;
 mongoose.connect(process.env.MONGO             
@@ -18,6 +21,8 @@ mongoose.connect(process.env.MONGO
 })
 app.use("/api/user" , userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRouter);
+
 
 app.listen(PORT , () => {
     console.log(`server running on port ${PORT}`);
---
+++ api/routes/post.route.js
@@ -0,0 +1,12 @@
+
+
+
+import express from 'express';
+import {create} from '../controller/post.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+const router = express.Router();
+
+
+router.post("/create", verifyToken, create)
+
+export default router;
