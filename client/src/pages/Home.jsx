c2d82a4b0b62877c8a0c7ba56f95b421bf44d168
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-04 22:48:47 +0300

    Add useEffect and useState hooks to fetch and display posts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,34 @@
-
-
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("/api/post/getPots");
+      const data = await res.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
   return (
     <div>
-      
+      <div className="flex flex-col gap-6 lg:p-28 p-3 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:txt-6xl">Welcome to my Blog</h1>
+        <p className="sm:text-sm text-xs text-gray-500">
+          Here you'll find a variety of articles and tutorials on topics such as
+          web development, software engineering, and programming languages.
+        </p>
+        <Link
+          to="/search"
+          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+        >
+          View all posts
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
