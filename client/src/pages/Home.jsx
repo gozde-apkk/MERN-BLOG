eac2f1c947e74597b60a2686a8ebb9cefc3bd39b
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-05 12:48:05 +0300

    Fix typo in API endpoint

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,18 +6,18 @@ const Home = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await fetch("/api/post/getPots");
+      const res = await fetch("/api/post/getposts");
       const data = await res.json();
       setPosts(data);
     };
     fetchPosts();
-  }, []);
+  }, [posts]);
   return (
     <div>
       <div className="flex flex-col gap-6 lg:p-28 p-3 max-w-6xl mx-auto">
         <h1 className="text-3xl font-bold lg:txt-6xl">Welcome to my Blog</h1>
         <p className="sm:text-sm text-xs text-gray-500">
-          Here you'll find a variety of articles and tutorials on topics such as
+          Here  You&apos;ll find a variety of articles and tutorials on topics such as
           web development, software engineering, and programming languages.
         </p>
         <Link
