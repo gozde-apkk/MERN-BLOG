14f1ee10f07714d6c9e0448025a4f9a78a0c9cc5
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-05 19:27:04 +0300

    Add CallToAction and CommentSection components to PostPage

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,6 +3,8 @@
 import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import CommentSection from "../components/CommentSection";
 
 const PostPage = () => {
   const {postSlug} = useParams();
@@ -58,13 +60,17 @@ const PostPage = () => {
             <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
         <span className='italic'>
-          {post && (post.content.length / 1000).toFixed(0)} mins read
+          {post && (post.content?.length / 1000).toFixed(0)} mins read
         </span>
       </div>
       <div
         className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction/>
+      </div>
+      <CommentSection postId={post._id}/>
        </main>
     )
 }
