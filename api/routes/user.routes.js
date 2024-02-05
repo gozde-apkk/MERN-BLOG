a9864036bae2315bbaa41869193ed59d8b8b93c1
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-05 13:39:18 +0300

    Add getUsers route to user.routes.js

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -3,16 +3,16 @@
 import express from 'express';
 import { test } from '../controller/user.controller.js  ';
 import { verifyToken } from '../utils/verifyUser.js';
-import { deleteUser, signout, updateUser } from '../controller/user.controller.js';
+import { deleteUser, getUsers, signout, updateUser } from '../controller/user.controller.js';
 
 
 const router = express.Router();
 
 router.get('/test', test)
-
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 
 
 export default router;  
