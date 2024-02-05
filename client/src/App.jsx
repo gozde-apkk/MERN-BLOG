543ee782dbb64395b5af3b718db4650db51af9f9
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-05 20:37:34 +0300

    Add ScrollToTop component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,11 +12,13 @@ import CreatePost from "./components/CreatePost";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
 
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
